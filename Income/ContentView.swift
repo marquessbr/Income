4b84956cec7b199e6ded863285a17ac73025587b
Armando Sobrinho <asobrinho@almavivasolutions.com.br>
2025-06-09 07:05:38 -0300

    Initial Commit

---
+++ Income/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Income
+//
+//  Created by Armando Marques da Silva Sobrinho on 09/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Income/IncomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeApp.swift
+//  Income
+//
+//  Created by Armando Marques da Silva Sobrinho on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
